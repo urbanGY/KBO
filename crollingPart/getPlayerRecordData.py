e00db4d3c3e39bb13f4635bcef4d27f4fb477037
Yeong Jun Joo <juyj7282@gmail.com>
2019-03-22 15:46:39 +0900

    ncdinos real append

---
+++ crollingPart/getPlayerRecordData.py
@@ -0,0 +1,92 @@
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+'''
+doosan_b.csv
+eagles_b.csv
+kia_b.csv
+kt_b.csv
+lg_b.csv
+lotte_b.csv
+nc_b.csv
+nexen_b.csv
+samsung_b.csv
+sk_b.csv
+
+'''
+
+def get_html(url):
+    output = ""
+    response = requests.get(url)
+    if response.status_code == 200:
+        output = response.text
+    return output    
+
+def get_data(url):
+    text = get_html(url)
+    soup = BeautifulSoup(text, 'html.parser')
+    data = soup.find_all("td",{"class":"statdata"}) #가까운 table을 찾고 그 안에 모든 td를 추출, 반환형이 string 아님
+    return data
+
+def get_arr(data):
+    arr = []
+    for i in range(0,len(data)):
+            s = data[i].get_text()
+            if len(s) < 1:
+                continue
+            if s[len(s)-1].isdigit():
+                continue
+            else:
+                if data[i+20].get_text() == '' or data[i+20].get_text() == '0.000': #공백 전적 제거
+                    i += 26
+                    continue
+                a = []
+                for j in range(i+20,i+26):
+                    a.append(data[j].get_text())
+                arr.append(a)    
+                i += 26
+    if len(arr) != 0:            
+        del(arr[len(arr)-1]) #통산전적 제거           
+    return arr
+
+
+def make_attribute(arr):
+    length = len(arr)
+    if length == 1:
+        return arr[0]
+    else:
+        output = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        n = 0
+        for i in range(1,length+1):
+            n += i
+        for i in range(1,length):
+            for j in range(0,6):
+                output[j] += (float(arr[i-1][j])/n)*i        
+        for i in range(0,6):
+            output[i] += float(arr[length-1][i])*(1-float(n-length)/n)
+        for i in range(0,6):
+            output[i] = round(output[i],3);
+        return output
+    
+team = ['doosan_b.csv','eagles_b.csv','kia_b.csv','kt_b.csv','lg_b.csv','lotte_b.csv','nc_b.csv','nexen_b.csv','samsung_b.csv','sk_b.csv']
+for teamList in team:
+    r = open('batterList/'+teamList,mode='rt') #선수 명단의 맨 앞부분에 해당 팀명 들어가있어야함
+    list = r.read().splitlines()
+    r.close()
+    f = open('batterOutput/'+list[0]+'.csv',mode='wt',newline='')
+    url = "http://www.statiz.co.kr/player.php?opt=1&name="
+    fieldnames = ['이름','타율','출루','장타','ops','wOBA','wRC+']
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    for n in range(1,len(list)):
+        print(url+list[n])
+        data = get_data(url+list[n])
+        arr = get_arr(data)
+        if len(arr) != 0:
+            attr = make_attribute(arr)   
+            csv_writer.writerow({'이름':list[n], '타율':attr[0], '출루':attr[1], '장타':attr[2], 'ops':attr[3], 'wOBA':attr[4], 'wRC+':attr[5]})
+        else:
+            print(list[n]+' 크롤링 제대로 안됨')
+    f.close()
+
---
+++ crollingPart/teamList/ncdinos.py
@@ -0,0 +1,74 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+def get_data(allUrl, personUrl):
+    result = {'name': [], 'birth': []}
+    pidList = []
+    for url in allUrl:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        playerList = soup.find('table', {'class': 'tb_player_info'}).find_all('li')
+        for li in playerList:
+            pid = 0
+            string = str(li)
+            for i in range(0, len(string)):
+                try:
+                    pid = pid * 10 + int(string[i])
+                except ValueError:
+                    continue
+     
+            pidList.append(pid)
+    
+    for pid in pidList:
+        name = ""
+        birth = ""
+        params = {'pid' : pid}
+        response = requests.get(personUrl, params=params)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        ddList = soup.find('dl', {'class': 'pl_description'}).find_all('dd')
+        for dd in ddList:
+            string = str(dd)
+            index = string.split("<strong>")[1].split("</strong>")[0].strip()
+            if index == "이름":
+                name = string.split(":")[1].split('/')[0].strip()
+            elif index == "생년월일":
+                birth = string.split(":")[1].split('<')[0].strip()
+        
+        birth = birth.replace("년 ", '-').replace("월 ", "-").replace("일", '').strip()
+
+        result['name'].append(name)
+        result['birth'].append(birth)
+    
+    return result
+
+pitcherUrl = ['http://ncdinos.com/player/pitcher']
+batterUrl = ['http://ncdinos.com/player/catcher', 'http://ncdinos.com/player/infielder', 'http://ncdinos.com/player/outfielder']
+personUrl = 'http://ncdinos.com/player/person'
+
+r = open('./ncdinos/pitcher.csv', 'wt', newline='')
+fieldnames = ['name', 'birth']
+
+pitcherList = get_data(pitcherUrl, personUrl)
+batterList = get_data(batterUrl, personUrl)
+
+print(pitcherList)
+print(batterList)
+
+writer = csv.DictWriter(r, fieldnames=fieldnames)
+writer.writeheader()
+
+for i in range(0, len(pitcherList['name'])):
+    writer.writerow({'name': pitcherList['name'][i], 'birth': pitcherList['birth'][i]})
+
+r.close()
+
+r = open('./ncdinos/batter.csv', 'wt', newline='')
+
+writer = csv.DictWriter(r, fieldnames=fieldnames)
+writer.writeheader()
+
+for i in range(0, len(batterList['name'])):
+    writer.writerow({'name': batterList['name'][i], 'birth': batterList['birth'][i]})
+
+r.close()
