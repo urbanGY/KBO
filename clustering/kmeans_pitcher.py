ccbd4a4794462f2d2ec9a583e20740c54544b410
Yeong Jun Joo <juyj7282@gmail.com>
2019-05-07 20:58:08 +0900

    투수 데이터 추가, 조금더 데이터 추가해서 군집화 테스트 더하기

--- clustering/kmeans_pitcher.py
+++ clustering/kmeans_pitcher.py
@@ -3,37 +3,27 @@ import pandas as pd
 import numpy as np
 import csv
 
-kmeans = KMeans(n_clusters=10, n_init = 2000)
+headerList = ['이름', '피안타', '피2루타', '피3루타', '피홈런', 'FIP','K/9','BB/9','삼진율','사구율','PFR','피안타율', '피장타율', '경기당이닝', 'P/PA', '뜬공/땅볼', '장타/안타']
+df = pd.DataFrame(columns=(headerList[1], headerList[2], headerList[3], headerList[4], headerList[5], headerList[6], headerList[7], headerList[8], headerList[9], headerList[10], headerList[11], headerList[12], headerList[13], headerList[14], headerList[15], headerList[16]))
 
-headerList = ['이름', '홈런%', '볼넷%', '삼진%', '절대장타율', '타석','안타','2루타','3루타','홈런','볼넷','삼진', '타율', '출루율', '장타율', '뜬/땅', 'RAA']
-df = pd.DataFrame(columns=(headerList[5], headerList[6], headerList[7], headerList[8], headerList[9], headerList[10], headerList[11]))
-
-f = open('../crollingPart/test.csv', 'rt')
+f = open('../crollingPart/PitcherData.csv', 'rt')
 reader = csv.reader(f)
 i = 0
 name = []
 
 for line in reader:
-    if len(line) > 0 and line[0] != '이름':
-        index = i
-        for j in range(0, len(name)):
-            if name[j] == line[0]:
-                index = j
-
-        if index == i :
-            name.append(line[0])
-            df.loc[i] = [float(line[6]), float(line[7]), float(line[8]), float(line[9]), float(line[10]), float(line[11]), float(line[12])]
-            i = i + 1
-        else:
-            for j in range(6, 13):
-                df.loc[index][j - 6] = df.loc[index][j - 6] + float(line[j])
+    if len(line) > 0 and line[0] != '이름' and int(line[1]) == 2018:
+        name.append(line[0])
+        df.loc[i] = [float(line[2]), float(line[3]), float(line[4]), float(line[5]), float(line[6]), float(line[7]), float(line[8]), float(line[9]), float(line[10]), float(line[11]), float(line[12]), float(line[13]), float(line[14]), float(line[15]), float(line[16]), float(line[17])]
+        i = i + 1
 
 f.close()
 
 
+kmeans = KMeans(n_clusters=6, n_init = 2000)
+
 kmeans.fit(df)
 factor = kmeans.predict(df)
-
 result = []
 zxczx = np.bincount(factor)
 
@@ -48,7 +38,7 @@ for zcv in result:
         if maxLen < len(zcv):
                 maxLen = len(zcv)
         print(zcv)
-"""
+
 f = open('./clusterOutput/classification_pitcher.csv', mode = 'wt', newline = '')
 field = []
 for i in range(0, len(result)):
@@ -67,5 +57,5 @@ for i in range(0, maxLen):
         csv_writer.writerow(tmp)
 
 f.close()                
-"""
+
 
