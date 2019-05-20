a7b16ad245628ecd13fa8b6936f54d42a5a84e56
Yeong Jun Joo <juyj7282@gmail.com>
2019-05-20 17:32:26 +0900

    4번째 AgglomerativeClustering 사용

--- clustering/AgglomerativeClustering_pitcher.py
+++ clustering/AgglomerativeClustering_pitcher.py
@@ -1,28 +1,28 @@
 from sklearn.cluster import AgglomerativeClustering
-
 import pandas as pd
 import numpy as np
 import csv
-agg = AgglomerativeClustering(n_clusters=8)
 
-headerList = ['이름', 'G', '타석', '타수', '득점', '안타','2타','3타','홈런','루타','타점','볼넷', '사구', '고4', '삼진', '타율', '출루', '장타', 'OPS']
-df = pd.DataFrame(columns=(headerList[15:]))
+headerList = ['이름', '출장', '완투', '완봉', '선발', '승','패','세','홀드','이닝','실점','자책', '타자', '안타', '홈런', '볼넷', '고4', '사구', '삼진', '보크', '폭투', 'WHIP', 'ERA+', 'FIP+']
+df = pd.DataFrame(columns=(headerList[1:]))
 
-f = open('../crollingPart/batter.csv', 'rt')
+f = open('../crollingPart/pitcher.csv', 'rt')
 reader = csv.reader(f)
 i = 0
 name = []
 
 for line in reader:
-    if len(line) > 0 and line[0] != '이름' and float(line[2]) >= 200:
+    if len(line) > 0 and line[0] != '이름' and float(line[9]) >= 50:
         name.append(line[0])
         tmp = []
-        for j in range(15, len(line)):
-            tmp.append(float(line[j]))
+        for j in range(1, len(line)):
+                tmp.append(float(line[j]))
         df.loc[i] = tmp
         i = i + 1
+
 f.close()
 
+agg = AgglomerativeClustering(n_clusters=7)
 
 factor = agg.fit_predict(df)
 
@@ -38,12 +38,11 @@ for index in range(0, i):
 
 maxLen = 0
 for zcv in result:
-        if maxLen < len(zcv):
-                maxLen = len(zcv)
+    if maxLen < len(zcv):
+        maxLen = len(zcv)
         print(zcv)
 
-
-f = open('./clusterOutput/classification_batter_4.csv', mode = 'wt', newline = '')
+f = open('./clusterOutput/classification_pitcher_4.csv', mode = 'wt', newline = '')
 field = []
 for i in range(0, len(result)):
         field.append(i)
@@ -60,4 +59,4 @@ for i in range(0, maxLen):
                         tmp[j] = result[j][i]
         csv_writer.writerow(tmp)
 
-f.close()  
+f.close()
